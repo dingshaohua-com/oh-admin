5e792317bdee764af27e4ea81d4a72f681f5eb72
丁少华 <960423114@qq.com>
2026-01-04 02:25:49 +0800

    fix: 增加一些路由

--- apps/admin/src/router/index.ts
+++ apps/admin/src/router/index.ts
@@ -7,21 +7,22 @@ export const router = createHashRouter([
     path: '/login',
     Component: lazy(() => import('../pages/login')),
   },
+  {
+    path: '/home',
+    Component: lazy(() => import('../pages/home')),
+  },
     {
-      path: '/home',
-      Component: lazy(() => import('../pages/home')),
+      path: '/',
+      Component: lazy(() => import('../pages/demo')),
+      // Component: Layout,
+      // children: [
+      //   { index: true, Component: lazy(() => import('@/pages/welcome')) },
+      //   { path: 'users', Component: lazy(() => import('@/pages/users')) },
+      //   { path: 'content', Component: lazy(() => import('@/pages/content')) },
+      //   { path: 'settings', Component: lazy(() => import('@/pages/settings')) },
+      //   { path: 'settings/security', Component: lazy(() => import('@/pages/settings/security')) },
+      // ],
     },
-  //   {
-  //     path: '/',
-  //     Component: Layout,
-  //     children: [
-  //       { index: true, Component: lazy(() => import('@/pages/welcome')) },
-  //       { path: 'users', Component: lazy(() => import('@/pages/users')) },
-  //       { path: 'content', Component: lazy(() => import('@/pages/content')) },
-  //       { path: 'settings', Component: lazy(() => import('@/pages/settings')) },
-  //       { path: 'settings/security', Component: lazy(() => import('@/pages/settings/security')) },
-  //     ],
-  //   },
 ]);
 
 export default router;
