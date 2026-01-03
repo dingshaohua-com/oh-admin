f7ec0e804f5bc6e0196f37b860af5216766a286d
丁少华 <960423114@qq.com>
2026-01-04 02:39:07 +0800

    fix: 继承turborepo并修改admin打包位置

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # oh-admin
 
+## 使用turbo
+安装 ` pnpm add --D -w turbo`，并在根目录下创建 `turbo.json`.
+这样就初始化好了 turborepo
+
+
--- apps/admin/rsbuild.config.ts
+++ apps/admin/rsbuild.config.ts
@@ -4,4 +4,9 @@ import { pluginReact } from '@rsbuild/plugin-react';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    distPath: {
+      root: '../../dist',
+    },
+  },
 });
